7df96f9190110a557df0655f7680de9a74c3bd87
Daniel Ribuffo <dribuffo@gmail.com>
2022-03-29 15:50:02 -0700

    bug fixes, and added test routes, added code to prep for post mvp

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,5 @@
 //depdencies
 const express = require("express");
-const req = require("express/lib/request");
 const router = express.Router();
 const Joi = require("joi");
 const bcrypt = require('bcrypt');
@@ -9,35 +8,17 @@ const bcrypt = require('bcrypt');
 const { User } = require("../models/User");
 
 router.post("/", async(req, res) => {
-    try{
-        const{error} = validate(req.body);
-        if(error)
-            return res.status(400).send({message: error.details[0].message})
-        
-            const user = await User.findOne({email:req.body.email});
-            if(!user)
-                return res.status(401).send({message: "Invalid Email or Password"});
-            
-            const validPassword = await bcrypt.compare(
-                req.body.password, user.password
-            );
-            if(!validPassword)
-                return res.status(401).send({message: "Invalid Email or Password"})
-            
-            const token = user.generateAuthToken();
-            res.status(200).send({message: "Logged in successfully"})
-    } catch(error) {
-        res.status(500).send({message: "Internal Server Error"})
-    }
-})
+    const user = await User.findOne({email: req.body.email });
+    if(!user)
+        return res.status(400).send({message: "Invalid e-mail or password"});
+    
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if(!validPassword)
+        return res.status(400).send({message: "Invalid e-mail or password"});
 
-const validate = (data) => {
-    const schema = Joi.object({
-        email: Joi.string().email().required().label("Email"),
-        password: Joi.string().required().label("Password")
-    });
-    return schema.validate(data);
-}
+    const token = user.generateAuthToken();
+    res.status(200).send({data: token, message: "Signing in please wait"})
+})
 
 //export
 module.exports = router;
--- controllers/userController.js
+++ controllers/userController.js
@@ -6,26 +6,53 @@ const bcrypt = require("bcrypt");
 //imports
 const {User, validate} = require("../models/User")
 
+//POST MVP
+const auth = require("../middleware/authMiddleware")
+const admin = require("../middleware/admin")
+const validObjectId = require("../middleware/validObjectId")
+
 //creates a new user
 router.post("/", async (req, res) => {
-    try {
-        const {error} = validate(req.body)
-        if(error)
-            return res.status(400).send({ message: error.details[0].message})
+    const {error} = validate(req.body)
+    if(error)
+        return res.status(400).send({ message: error.details[0].message})
 
-        const user = await User.findOne({email: req.body.email})
-        if(user)
-            return res.status(409).send({ message: "User with given e-mail already exists"})
+    const user = await User.findOne({email: req.body.email})
+    if(user)
+        return res.status(409).send({ message: "User with given e-mail already exists"})
         
-        const salt = await bcrypt.genSalt(Number(process.env.SALT))
-        const hashPassword = await bcrypt.hash(req.body.password, salt)
+    const salt = await bcrypt.genSalt(Number(process.env.SALT))
+    const hashPassword = await bcrypt.hash(req.body.password, salt)
 
-        await new User({...req.body, password: hashPassword}).save();
-        res.status(201).send({message: "User created successfully"})
-    } catch (error) {
-        res.status(500).send({message: "Internal Server Error"})
-    }
-})
+    let newUser = await User({
+        ...req.body,
+        password: hashPassword
+    }).save();
+
+    newUser.password = undefined;
+    newUser._v = undefined;
+    res.status(201).send({data: newUser, message: "User created successfully"})
+});
+
+
+//POST MVP ROUTES - POSTMAN ONLY
+//get all users route
+// router.get('/', async (req, res) => {
+//     const users = await User.find().select("-password -_v");
+//     res.status(200).send({data: users});
+// });
+
+// //get user by ID
+// router.get("/:id", async (req, res) =>  {
+//     const user = await User.findById(req.params.id).select("-password -_v")
+//     res.status(200).send({data:user})
+// });
+
+// //delete user by ID
+// router.delete("/:id", async(req, res) =>{
+//     await User.findByIdAndDelete(req.params.id);
+//     res.status(200).send({message: "Successfully deleted user"})
+// });
 
 //export
 module.exports = router;
